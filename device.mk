f9981a51033e35bbc5a21937f172a4a2af59c51c
Cyborg2017 <wangyang0987654@gmail.com>
2018-09-22 15:37:14 +0800

    c7ltechn: Commonize shared stuff
    Change-Id: I91e41054127ed008f4a6f619a365ec38b0213b5d

--- device.mk
+++ device.mk
@@ -6,11 +6,6 @@ $(call inherit-product, device/samsung/c7lte-common/c7lte.mk)
 
 LOCAL_PATH := device/samsung/c7ltechn
 
-# Data configs
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
-    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
-    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml
 
 # Security
 ADDITIONAL_DEFAULT_PROPERTIES += \
